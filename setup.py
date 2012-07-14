efefa287ec3bef1710c62dc6f96f961c5ad786e7
Timothy Asp <tim@ifixit.com>
2012-07-13 17:23:59 -0700

    More project formatting

--- setup.py
+++ setup.py
@@ -1,26 +1,27 @@
 #!/usr/bin/env python
-#from distutils.core import setup
-from setuptools import setup, find_packages
+from distutils.core import setup
+#from setuptools import setup, find_packages
 import ConfigParser, os
 
 # Read version.conf and use the version #.
 config = ConfigParser.ConfigParser()
 config.readfp(open('version.conf'))
 
-setup(name="smartytophp",
-      version=config.get('app:main', 'version'),
-      description="Converts Smarty templates into PHP templates.",
-      author="Modified By: Timothy Asp Authored By: Ben Coe",
-      author_email="tim@ifixit.com",
-      entry_points={
-          'console_scripts': [
-              'smartytotwig = smartytotwig.main:main'
-          ]
-      },
-      url="git@github.com:ifixit/smartytophp.git",
-      packages = find_packages(),
-      include_package_data=True,
-      setup_requires=['setuptools-git'],
-      install_requires = ['simplejson==2.1.1'],
-      tests_require=['nose', 'coverage'],
-)
+setup(
+    name="smartytophp",
+    version=config.get('app:main', 'version'),
+    author="Timothy Asp",
+    author_email="tim@ifixit.com",
+    description="Converts Smarty templates into PHP templates. Heavily
+    modified fork of FreshBooks SmartyToTwig.",
+    long_description=open('README.md').read(),
+    entry_points={
+        'console_scripts': [
+            'smartytotwig = smartytotwig.main:main'
+        ]
+    },
+    url="git@github.com:ifixit/smartytophp.git",
+    packages = ['smartytophp', 'smartytophp.test'],
+    include_package_data=True,
+    install_requires = ['simplejson==2.1.1'],
+k)
