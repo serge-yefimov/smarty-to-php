6c3c813a64bb2c8fd1d2be19c9651a5d0c628fca
Timothy Asp <tim@ifixit.com>
2012-08-06 09:59:15 -0700

    Updated setup scripts

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 smarty-to-php
 =============
 
-Converting Smarty back to PHP
+Kill Smarty. 
+
+First, log in to cominor and make sure everything is fetched and up-to-date.
+Then check out the `smarty-to-php` branch which is where we will be working on
+the conversion.
+
+Begin the process with:
+   
+   git clone git@github.com:iFixit/smarty-to-php.git
+   cd smart-to-php
+   sudo python26 setup.py install
 
 Example run: `python smartytophp/main.py --smarty-file=smartytophp/test/vars.tpl --phtml-file=vars.phtml`
--- setup.py
+++ setup.py
@@ -12,15 +12,11 @@ setup(
     version=config.get('app:main', 'version'),
     author="Timothy Asp",
     author_email="tim@ifixit.com",
-    description="Converts Smarty templates into PHP templates. Heavily modified fork of FreshBooks SmartyToTwig.",
-    long_description=open('README.md').read(),
+    packages=['smartytophp'],
+    url="git@github.com:ifixit/smarty-to-php.git",
     entry_points={
-        'console_scripts': [
-            'smartytotwig = smartytotwig.main:main'
-        ]
+        'console_scripts': [ 'smartytophp = smartytophp.main:main' ],
     },
-    url="git@github.com:ifixit/smartytophp.git",
-    packages = ['smartytophp', 'smartytophp.test'],
-    include_package_data=True,
-    install_requires = ['simplejson==2.1.1'],
+    description="Converts Smarty templates into PHP templates. Heavily modified fork of FreshBooks SmartyToTwig.",
+    long_description=open('README.md').read()
 )
