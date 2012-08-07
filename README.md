718448f795fc6c1d766aa6bffd000764e4139698
Timothy Asp <tim@ifixit.com>
2012-08-06 23:37:53 -0700

    Set up automated tests and updated the documentation

--- README.md
+++ README.md
@@ -14,3 +14,25 @@ Begin the process with:
    sudo python26 setup.py install
 
 Example run: `python smartytophp/main.py --smarty-file=smartytophp/test/vars.tpl --phtml-file=vars.phtml`
+
+Testing
+=======
+
+Tests are located in smartytophp/test.  Each test consists of a `.tpl` and a
+`.phtml` file and the contents of each `.tpl` file are fed to the parser and
+the output is diffed against the `.phtml` file which is the expected output
+from the input.  
+
+Running the tests
+=================
+
+   ./runtests
+
+Generating tests
+================
+To generate tests for a part of the smarty grammar, add whatever the the failing piece of code into a seperate file in
+`./smartytophp/tests`.  For example `mv template_from_hell.tpl ~/path/to/smarty-to-php/smartytophp/tests`. 
+
+Then run `./convert template_from_hell` which converts file, or will try to, and generates a broken `.phtml`.  But
+that's ok. We can fix it. Just convert the rest by hand and start fiddling with
+the grammar and tree walker (lexer(?) sort of?) till your tests pass. 
--- smartytophp/test_smarty_grammar.py
+++ smartytophp/test_smarty_grammar.py
@@ -1,90 +1,60 @@
-from smartytotwig.tree_walker import TreeWalker
-import unittest, time, smartytotwig
+from tree_walker import TreeWalker
+from smarty_grammer import smarty_language
+import unittest, time, os
+
+from main import parse_file, parse_string
 
 class TestSmartyGrammar(unittest.TestCase):
     """
     It's easy to screw up other rules when modifying the underlying grammar.
     These unit tests test various smarty statements, to make refactoring the grammar more sane.
     """
-   
-    def test_if_statement(self):
+
+    def setUp(self):
+        self.outputFiletype = '.phtml'
+        self.inputFiletype = '.tpl'
+        self.testDir = 'test'
+        self.input_files = self.output_files = []
+
+        for dirname, dirnames, filenames in os.walk(self.testDir):
+            for filename in filenames:
+                if filename.endswith(self.inputFiletype):
+                    self.input_files.append(self.testDir + '/' + filename)
+                if filename.endswith(self.outputFiletype):
+                    self.output_files.append(self.testDir + '/' + filename)
+
+
+    def test_statement(self):
         """
         Test several different types of if statements.
         """
-        # Test an if statement (no else or elseif)
-        ast = smartytotwig.parse_string("{if !foo or foo.bar or foo|bar:foo['hello']}\nfoo\n{/if}")
-        tree_walker = TreeWalker(ast)
-        self.assertEqual(tree_walker.code, "{% if not foo or foo.bar or foo|bar(foo['hello']) %}\nfoo\n{% endif %}")
-        
-        # Test an an if with an else and a single logical operation.
-        ast = smartytotwig.parse_string("{if foo}\nbar\n{else}\nfoo{/if}")
-        tree_walker = TreeWalker(ast)
-        self.assertEqual(tree_walker.code, "{% if foo %}\nbar\n{% else %}\nfoo{% endif %}")
+        print self.input_files
+        for filename in self.input_files:
+            fn = os.path.basename(filename)
+            output_file = os.path.splitext(fn)[0] + self.outputFiletype
+            print "outputfile: ",output_file
+            output = open(self.testDir + '/' + output_file)
 
-        # Test an an if with an else and an elseif and two logical operations.
-        ast = smartytotwig.parse_string("{if foo and awesome.string|banana:\"foo\\\" $a\"}\nbar\n{elseif awesome.sauce[1] and blue and 'hello'}\nfoo{/if}")
-        tree_walker = TreeWalker(ast)
-        self.assertEqual(tree_walker.code, "{% if foo and awesome.string|banana(\"foo\\\" %s\"|format(a)) %}\nbar\n{% elseif awesome.sauce[1] and blue and \'hello\' %}\nfoo{% endif %}")
+            print "inputfile: ",filename 
+            statement = open(filename)
 
-        # Test an if with an elseif and else clause.
-        ast = smartytotwig.parse_string("{if foo|bar:3 or !foo[3]}\nbar\n{elseif awesome.sauce[1] and blue and 'hello'}\nfoo\n{else}bar{/if}")
-        tree_walker = TreeWalker(ast)
-        self.assertEqual(tree_walker.code, "{% if foo|bar(3) or not foo[3] %}\nbar\n{% elseif awesome.sauce[1] and blue and 'hello' %}\nfoo\n{% else %}bar{% endif %}")
-        
-        # Test an an if statement with parenthesis.
-        ast = smartytotwig.parse_string("{if (foo and bar) or foo and (bar or (foo and bar))}\nbar\n{else}\nfoo{/if}")
-        tree_walker = TreeWalker(ast)
-        self.assertEqual(tree_walker.code, "{% if (foo and bar) or foo and (bar or (foo and bar)) %}\nbar\n{% else %}\nfoo{% endif %}")
+            # Test an if statement (no else or elseif)
+            ast = parse_string(statement.read())
+            tree_walker = TreeWalker(ast)
+            testOutput = tree_walker.code
+            self.assertEqual(testOutput, output.read())
 
-        # Test an an elseif statement with parenthesis.
-        ast = smartytotwig.parse_string("{if foo}\nbar\n{elseif (foo and bar) or foo and (bar or (foo and bar))}\nfoo{/if}")
-        tree_walker = TreeWalker(ast)
-        self.assertEqual(tree_walker.code, "{% if foo %}\nbar\n{% elseif (foo and bar) or foo and (bar or (foo and bar)) %}\nfoo{% endif %}")
 
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(TestSmartyGrammer))
+    return suite
         
-    def test_function_statement(self):
-        """
-        Test Smarty's function? statement:
-        
-        {foo arg=bar}
-        """
-        # Test a a simple function statement.
-        ast = smartytotwig.parse_string("{foo arg1=bar arg2=3}")
-        tree_walker = TreeWalker(ast)
-        self.assertEqual(tree_walker.code, "{{['arg1': bar, 'arg2': 3]|foo}}")
-        
-        # Test a a simple function statement with object and array arguments.
-        ast = smartytotwig.parse_string("{foo arg1=bar[1] arg2=foo.bar.foo arg3=foo.bar[3] arg4=foo.bar.awesome[3] }")
-        tree_walker = TreeWalker(ast)
-        self.assertEqual(tree_walker.code, "{{['arg1': bar[1], 'arg2': foo.bar.foo, 'arg3': foo.bar[3], 'arg4': foo.bar.awesome[3]]|foo}}")
-        
-        # Test a function statement with modifiers in in the parameters.
-        ast = smartytotwig.parse_string("{foo arg1=bar[1]|modifier arg2=foo.bar.foo arg3=foo.bar[3]|modifier:array[0]:\"hello $foo \" arg4=foo.bar.awesome[3]|modifier2:7:'hello':\"hello\":\"`$apple.banana`\"}")
-        tree_walker = TreeWalker(ast)
-        self.assertEqual(tree_walker.code, "{{[\'arg1\': bar[1]|modifier, \'arg2\': foo.bar.foo, \'arg3\': foo.bar[3]|modifier(array[0], \"hello %s \"|format(foo)), \'arg4\': foo.bar.awesome[3]|modifier2(7, \'hello\', \"hello\", \"%s\"|format(apple.banana))]|foo}}")
-        
-    def test_for_statement(self):
-        """
-        Test Smarty's for statement:
-        
-        {foreach foo}
-        {foreachelse}
-        {/foreach}
-        """
-        # Test a a simple foreach statement.
-        ast = smartytotwig.parse_string("{foreach item=bar from=foo }{/foreach}")
-        tree_walker = TreeWalker(ast)
-        self.assertEqual(tree_walker.code, "{% for bar in foo %}{% endfor %}")
-        
-        # Test a more complex foreach statement.
-        ast = smartytotwig.parse_string("{foreach item='bar'    name=snuh key=\"foobar\" from=foo[5].bar[2]|hello:\"world\":\" $hey \" }bar{/foreach}")
-        tree_walker = TreeWalker(ast)
-        self.assertEqual(tree_walker.code, "{% for bar in foo[5].bar[2]|hello(\"world\", \" %s \"|format(hey)) %}bar{% endfor %}")
-        
-        # Test a for statement with a foreachelse clause.
-        ast = smartytotwig.parse_string("{foreach item='bar'    name=snuh key=\"foobar\" from=foo.bar[2]|hello:\"world\":\" $hey \" }bar{foreachelse}{if !foo}bar{/if}hello{/foreach}")
-        tree_walker = TreeWalker(ast)
-        self.assertEqual(tree_walker.code, "{% for bar in foo.bar[2]|hello(\"world\", \" %s \"|format(hey)) %}bar{% else %}{% if not foo %}bar{% endif %}hello{% endfor %}")
-
 if __name__ == '__main__':
     unittest.main()
+    """
+    suiteFew = unittest.TestSuite()
+    suiteFew.addTest(TestSmartyGrammer("test_statement"))
+    unittest.TextTestRunner(verbosity=2).run(suite())
+    """
+
